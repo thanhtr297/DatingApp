62122ee2743c663f7877b3bb44204689c84ff216
thanhtr297 <thanh.tranminh.hanoi@gmail.com>
2024-01-15 16:44:59 +0700

    End of session 10

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,21 +2,41 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_model/Member';
+import { map } from 'rxjs/internal/operators/map';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
+  members: Member[] = [];
 
   baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    if(this.members.length>0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+       this.members = members
+       return members;
+      })
+    );
   }
 
   getMember(username: string) {
+    const member = this.members.find(x=> x.username === username);
+    if(member !== undefined) return of (member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+  
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    )
   }
 }
